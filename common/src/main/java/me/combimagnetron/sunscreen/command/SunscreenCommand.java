f4f307ca0405666ac12275c922cf8ac11d76c5b9
Alec <alec@tikvout.nl>
2026-05-01 23:37:37 +0200

    fix: remove references to non existent class

--- common/src/main/java/me/combimagnetron/sunscreen/command/SunscreenCommand.java
+++ common/src/main/java/me/combimagnetron/sunscreen/command/SunscreenCommand.java
@@ -1,18 +1,7 @@
 package me.combimagnetron.sunscreen.command;
 
-import me.combimagnetron.passport.util.data.Identifier;
-import me.combimagnetron.passport.util.math.Vec2i;
-import me.combimagnetron.sunscreen.SSCustomGames;
 import me.combimagnetron.sunscreen.SunscreenLibrary;
-import me.combimagnetron.sunscreen.example.BiddingHouseMenuTemplate;
-import me.combimagnetron.sunscreen.neo.TestMenuTemplate;
-import me.combimagnetron.sunscreen.neo.ActiveMenu;
 import me.combimagnetron.sunscreen.neo.editor.EditorController;
-import me.combimagnetron.sunscreen.neo.editor.template.EditorMenuTemplate;
-import me.combimagnetron.sunscreen.neo.editor.template.EditorStartOverviewMenuTemplate;
-import me.combimagnetron.sunscreen.neo.element.ElementLike;
-import me.combimagnetron.sunscreen.neo.property.Position;
-import me.combimagnetron.sunscreen.neo.session.Session;
 import me.combimagnetron.sunscreen.user.SunscreenUser;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
@@ -42,12 +31,6 @@ public class SunscreenCommand {
         SunscreenUser<?> user = SunscreenLibrary.library().users().user(actor.uniqueId()).orElseThrow();
         EditorController controller = new EditorController(user);
         controller.start();
-    }
-
-    @Subcommand("test")
-    public void test(@NotNull CommandActor actor) {
-        SunscreenUser<?> user = SunscreenLibrary.library().users().user(actor.uniqueId()).orElseThrow();
-        user.open(new SSCustomGames());
     }
 
 }
